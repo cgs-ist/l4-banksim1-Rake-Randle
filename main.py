7e685bcd821221d92279ba59821568394dacd602
Jake <jake.handel@cgs.act.edu.au>
2024-05-20 15:24:53 +1000

    finalise the bank code

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+from getpass import getpass
 # Non OOP
 # Bank Version 1
 # Single Account
 
-accountName = 'Joe'
+name = str(input("What is your name: "))
+accountName = name.lower()
 accountBalance = 100
-accountPassword = 'soup'
+accountPassword = str(getpass("What is your password: "))
+
+if accountName == 'guha':
+    accountBalance = 0
+    print("Get some money guhy")
+elif accountName == 'oliver':
+    accountBalance = 500
+    print("damn oli good to see you again") 
+elif accountName == 'jake':
+       accountBalance = 10000
+       print("Hi myself")
 
 while True:
-    print('Press b to get the balance')
+    print('\nPress b to get the balance')
     print('Press d to make a deposit')
     print('Press w to make a withdrawl')
     print('Press s to show the account')    
@@ -18,9 +30,40 @@ while True:
     action = action[0]
     
     if action == 'b':
-        print('Get Balance:')
-        userPassword = input('Please enter your password: ')
+        print('\nGet Balance:')
+        userPassword = getpass('Please enter your password: ')
         if userPassword != accountPassword:
             print('Incorrect Password')
         else:
             print('Your balance is: ', accountBalance)
+    elif action == 'd':
+        deposit = float(input('\nEnter the amount you would like to deposit: '))
+        userPassword = getpass('Please enter your password: ')
+        if userPassword != accountPassword:
+            print('Incorrect Password')
+        else:
+            if deposit < 0:
+                print("Please enter a positive amount")
+            else:
+                accountBalance += deposit 
+    elif action == 'w':
+        withdrawl = float(input('\nEnter the amount you would like to withdrawl: '))
+        userPassword = getpass('Please enter your password: ')
+        if userPassword != accountPassword:
+            print('Incorrect Password')
+        else:
+            if withdrawl < 0:
+                print("Please enter a positive amount")
+            else:
+                accountBalance -= withdrawl
+    elif action == 's':
+        userPassword = getpass('\nPlease enter your password: ')
+        if userPassword != accountPassword:
+            print('Incorrect Password')
+        else:
+            print("Name:", accountName, 
+                "\nBalance:", accountBalance,
+                "\nPassword:", accountPassword)      
+    elif action == 'q':
+        print("\nThanks for your service", accountName)
+        exit()
